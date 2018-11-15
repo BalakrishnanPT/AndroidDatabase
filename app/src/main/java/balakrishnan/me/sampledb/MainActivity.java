e042e99f1ed990078a6b4f878ff2acbc9e742917
balakrishnanfn <bala@nfnlabs.in>
2018-11-15 17:31:36 +0530

    Realm Migration is implemented for single Model.

--- app/src/main/java/balakrishnan/me/sampledb/MainActivity.java
+++ app/src/main/java/balakrishnan/me/sampledb/MainActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.Objects;
+
 import balakrishnan.me.sampledb.Module.DomesticAnimalsModule;
 import balakrishnan.me.sampledb.model.Cat;
 import balakrishnan.me.sampledb.model.Dog;
-import io.realm.OrderedCollectionChangeSet;
-import io.realm.OrderedRealmCollectionChangeListener;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
@@ -17,10 +17,16 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Realm realm;
 
+    /**
+     * There is no auto increment annotation in Realm hence we have to do it manually
+     *
+     * @param c Class get primary key
+     * @return unique primary value
+     */
     public static int getPrimaryKey(Class c) {
         Realm realm = Realm.getDefaultInstance();
 
-        String primaryKeyFied = realm.getSchema().get(c.getSimpleName()).getPrimaryKey();
+        String primaryKeyFied = Objects.requireNonNull(realm.getSchema().get(c.getSimpleName())).getPrimaryKey();
         if (realm.where(c).max(primaryKeyFied) == null)
             return 1;
         int value = realm.where(c).max(primaryKeyFied).intValue();
@@ -35,58 +41,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void realmSetup() {
+
+        //DB Configuration we can use default configuration or custom configurations
         RealmConfiguration config = new RealmConfiguration.Builder()
                 .name(getPackageName() + ".realm")
-                .schemaVersion(2)
+                .schemaVersion(3)
+                .migration(new Migration())
                 .modules(new DomesticAnimalsModule())
                 .build();
 
         realm = Realm.getInstance(config);
 
+        //Query
         final RealmResults<Dog> puppies = realm.where(Dog.class).findAll();
 
         Log.d(TAG, "realmSetup: number of dogs" + puppies.size());
 
-        puppies.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<Dog>>() {
-            @Override
-            public void onChange(RealmResults<Dog> results, OrderedCollectionChangeSet changeSet) {
-                // Query results are updated in real time with fine grained notifications.
-                Log.d(TAG, "onChange: Puppies" + changeSet.getInsertions());// => [0] is added.
-            }
+        puppies.addChangeListener((results, changeSet) -> {
+            // Query results are updated in real time with fine grained notifications.
+            Log.d(TAG, "onChange: Puppies" + changeSet.getInsertions());// => [0] is added.
         });
 
         final RealmResults<Cat> kitten = realm.where(Cat.class).findAll();
         Log.d(TAG, "realmSetup: number of kitten" + kitten.size()); // => 0 because no dogs have been added to the Realm yet// Listeners will be notified when data changes
 
-        kitten.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<Cat>>() {
-            @Override
-            public void onChange(RealmResults<Cat> results, OrderedCollectionChangeSet changeSet) {
-                // Query results are updated in real time with fine grained notifications.
-                for (Cat cat : results) {
-                    Log.d(TAG, "onChange: Puppies" + cat.getName());
-                }
+        kitten.addChangeListener((results, changeSet) -> {
+            // Query results are updated in real time with fine grained notifications.
+            for (Cat cat : results) {
+                Log.d(TAG, "onChange: Puppies" + cat.getName());
             }
         });
-        addAnimals(4);
-        realm.executeTransaction(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                Dog dog = realm.createObject(Dog.class);
-                dog.setName("Fido");
-                dog.setAge(5);
-            }
+        addCat(1);
+
+        //CreateObject example
+        realm.executeTransaction(realm -> {
+            Dog dog = realm.createObject(Dog.class);
+            dog.setName("Fido");
+            dog.setAge(5);
         });
     }
 
-    public void addAnimals(final int count) {
-        realm.executeTransaction(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                for (int i = 0; i < count; i++) {
-                    Cat cat = realm.createObject(Cat.class);
-                    cat.setName("Cat " + i);
-                }
-            }
-        });
+    /**
+     * This function explains copyToRealm
+     *
+     * @param count
+     */
+    public void addCat(final int count) {
+        // Copy the object to Realm. Any further changes must happen on realmUser
+        realm.beginTransaction();
+        for (int i = 0; i < count; i++) {
+            Cat temp = new Cat();
+            temp.setName("Cat " + i);
+            realm.copyToRealm(temp);
+        }
+        realm.commitTransaction();
     }
 }
--- app/src/main/java/balakrishnan/me/sampledb/model/Cat.java
+++ app/src/main/java/balakrishnan/me/sampledb/model/Cat.java
@@ -5,6 +5,15 @@ import io.realm.RealmObject;
 public class Cat extends RealmObject {
 
     private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
